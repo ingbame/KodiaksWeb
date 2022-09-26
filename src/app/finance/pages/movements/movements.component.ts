e93b2de544816964fd38fa20a7ff7a8f0fc25e13
Baruch Medina <54144103+ingbame@users.noreply.github.com>
2022-09-26 01:02:57 -0500

    prueba de modales

--- src/app/finance/pages/movements/movements.component.ts
+++ src/app/finance/pages/movements/movements.component.ts
@@ -8,16 +8,23 @@ import { MovementEntity } from '../../models/movement';
 })
 export class MovementsComponent implements OnInit {
   total: number = 0.00;
+  movement: MovementEntity = new MovementEntity();
   movements: MovementEntity[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.movements.push({
+      movementDate: new Date(),
+      movementTypeKey: "Gto",
+      conceptDesc: "Ampayeo",
+      amount: 50.00
+    });
   }
   OpenAddMovementModel(): void {
 
   }
   onOpenDetailMovement(movement: any): void {
-
+    this.movement = movement;
   }
 }
